98b6016226aba614da9683806d30fd5602e8397c
Udoka <udochukwukaonyela@gmail.com>
2022-08-08 10:57:19 +0100

    exports

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 import icon from './favicon.ico';
 import vercel from './vercel.svg';
-import home from './home.svg';
+import Home from './Home.svg';
+import Mega from './megaphone.svg';
+import Account from './account.svg';
 
-export { icon, vercel, home };
+export { icon, vercel, Home, Mega, Account };
