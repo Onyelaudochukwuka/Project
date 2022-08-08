dec47647e761d6e5c05ebf149780f060d309ef8c
Udoka <udochukwukaonyela@gmail.com>
2022-08-08 10:57:14 +0100

    styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,41 @@
 import Image from "next/image";
-import React, { FC, useState } from "react";
-import Home from "../public/home.svg";
-import Account from "../public/account.svg";
-import Advert from "../public/megaphone.svg";
+import React, { FC, useEffect, useState } from "react";
+import { Home, Mega as Advert, Account } from "../public/index";
 import Link from "next/link";
 
 const Navbar: FC = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  const navHandler = () => {
-    setIsMobile (!isMobile)
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const navHandler = ():void => {
+    setIsMobile (!isMobile);
   };
-
+  useEffect(()=>{
+    console.log(window)
+  },[])
   return (
     <>
-      <div className="w-[8rem]  h-full absolute left-0 z-10 flex bg-white">
+      <div className={`${!isMobile ? `w-0`: `w-16` } transition-all duration-700 ease-in overflow-x-hidden  h-full min-h-screen absolute left-0 top-0 bottom-0 z-10 flex bg-white`}>
         <div className="h-full w-0 bg-[#429EBD] px-8 flex flex-col justify-center items-center gap-6 transition-all ease-in duration-1000"  >
-          <Link href="/home">
             <div className="h-[2.6rem] w-[2.6rem] bg-[#9FE7F5] rounded-full pt-2 cursor-pointer" onClick={() => setIsMobile (false)}>
+          <Link href="/home">
               <Home className="fill-[#053F5C] h-6 w-6 m-auto" />
-            </div>
           </Link>
+            </div>
 
-          <Link href="/account">
             <div className="cursor-pointer pt-2"  onClick={() => setIsMobile (false)}>
+          <Link href="/account">
               <Account className="w-12 h-12 fill-[#9FE7F5]" />
-            </div>
           </Link>
-          <Link href="/advert">
+            </div>
             <div className="h-[2.6rem] w-[2.6rem] bg-[#9FE7F5] rounded-full pt-2 cursor-pointer"  onClick={() => setIsMobile (false)}>
+          <Link href="/advert">
               <Advert className="fill-[#053F5C] h-6 w-6 m-auto" />
-            </div>
           </Link>
-        </div>
-        <div className="w-16 h-9  cursor-pointer bg-black" onClick={navHandler}>
-          <span className="w-14 h-1 bg-[#9FE7F5] block"></span>
-          <span className="w-12 h-1 bg-[#9FE7F5] block mt-2 ml-2"></span>
-          <span className="w-10 h-1 bg-[#9FE7F5] block mt-2 ml-4"></span>
+            </div>
         </div>
       </div>
+      <div className={`bg-transparent relative top-4 ${!isMobile? `left-4` : `left-20`} transition-all duration-700 ease-in`} onClick={navHandler}>
+          <span className="w-14 h-1 cursor-pointer rounded-full  bg-[#9FE7F5] block after:content-[''] after:relative before:w-12 after:h-1 after:bg-[#9FE7F5] after:block after:mt-1 after:float-right after:ml-auto before:content-[''] before:relative after:w-10 before:h-1 before:mt-2 before:float-right before:ml-auto before:bg-[#9FE7F5] before:block self-end before:self-end after:self-end before:rounded-full after:rounded-full"></span>
+        </div>
     </>
   );
 };
