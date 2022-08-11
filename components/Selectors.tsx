ebc4684424646af46224f93078332dd0236032ca
Udoka <udochukwukaonyela@gmail.com>
2022-08-11 13:08:08 +0100

    styling

--- components/Selectors.tsx
+++ components/Selectors.tsx
@@ -1,4 +1,5 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren, useId, useState } from 'react';
+import Link from 'next/link';
 type Pages = {
     heading: string,
     value: string[]
@@ -6,14 +7,19 @@ type Pages = {
 
 const Selectors = ({ pages }:any) => {
     const newArr: Pages[] = [...pages];
+    const Id = useId()
   return (
-      <div>
-          {newArr.map(({ heading, value }:Pages, i:number) =>
-              <div>
-                  <span>{heading}</span>
+      <div className="grid grid-cols-3 gap-4">
+          {newArr.map(({ heading, value }: Pages, i: number) => {
+              const [drop, setDrop] = useState<boolean>(false);
+              return (
+                  <div className="transition-[height] duration-300 ease-in">
+                      <span onBlur={()=>setDrop(false)} tabIndex={5} onClick={() => setDrop(!drop)} className="cursor-pointer bg-light p-3">{heading}</span>
                   
-                  {value.map((option) => <option>{option}</option>)}
-                </div>
+                      <div className={`flex flex-col gap-2 font-bold text-lg text-white ${drop? `h-full`: `h-0`} overflow-y-hidden transition-[height] duration-300 ease-in`}>{value.map((option) => <Link href={option} key={Id + option}>{option}</Link>)}</div>
+                  </div>
+              )
+          }
           )}
         </div>
   )
