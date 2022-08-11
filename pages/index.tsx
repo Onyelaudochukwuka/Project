a5930636ad33d1bba91bc0a18712f6671cc70d35
Udoka <udochukwukaonyela@gmail.com>
2022-08-11 12:37:30 +0100

    index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-
+import { Selectors } from "../components";
+type Pages = {
+  heading: string,
+  value: string[]
+}
+const Arr: Pages[] = [
+  {
+    heading: "Automobile",
+    value: ["Cars", "Bicycle", "Bus"]
+  }
+]
 const Home: NextPage = () => {
   return (
-    <div>
-      
-</div>
+    <div className="w-full mt-12 flex items-center justify-center h-full">
+      <Selectors pages={Arr} />
+    </div>
   );
 };
 
