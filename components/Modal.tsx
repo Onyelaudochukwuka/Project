945074d3092ed899202b692df91adeb858ff3637
Udoka <udochukwukaonyela@gmail.com>
2022-08-17 14:37:16 +0100

    styling modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react'
+import { Cancel } from "../public/icons"
 
-const Modal:FC = () => {
+const Modal:FC<{close: () => void}> = ({close}) => {
   return (
       <section className="absolute flex flex-1 justify-center items-center bg-gray-100/50 inset-0 z-20">
-          <div className="bg-white w-3/4 h-3/4 rounded-lg shadow-lg flex flex-col justify-center gap-12 items-center">
+          <div className="bg-white w-3/4 h-3/4 rounded-lg shadow-lg flex flex-col justify-center gap-12 items-center relative">
+              <Cancel onClick={()=> close()} className="w-6 h-6 absolute top-6 right-6 cursor-pointer"/>
           <select className="p-6 outline-2 bg-medium text-white">
             <option value={""}>select a state</option>
             <option value={"Otawwa"}>Otawwa</option>
