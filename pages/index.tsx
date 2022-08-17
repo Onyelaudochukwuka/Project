a0fa5c6dd6fe0fa4a2913c2955650d326624d2e3
Udoka <udochukwukaonyela@gmail.com>
2022-08-17 14:15:51 +0100

    added states and modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Selectors } from "../components";
+import { useState } from "react";
+import { Modal, Selectors } from "../components";
 import { Book, Dropdown, Adult, Car } from "../public/index";
+import isPopUp, { display } from "../utils/hooks/isPopUp";
 type Pages = {
   heading: string,
   value: string[],
   icon: any
 }
+
 const Arr: Pages[] = [
   {
     heading: "Adult",
@@ -40,9 +43,14 @@ const Arr: Pages[] = [
   }
 ]
 const Home: NextPage = () => {
+  const [display, setDisplay] = useState<boolean>(false);
+  const isPopUp = () => {
+    setDisplay(!display);
+  }
   return (
-    <div className="w-full mt-12 flex items-center justify-center h-full">
+    <div className="w-full mt-12 flex flex-col items-center justify-center h-full">
       <Selectors pages={Arr} />
+      <Modal />
     </div>
   );
 };
