48fa9aa1c9bb969dc53e67955cee3be6cf66ee56
Udoka <udochukwukaonyela@gmail.com>
2022-08-17 14:37:21 +0100

    created ads

--- pages/ads.tsx
+++ pages/ads.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { addCount, removeCount } from "../utils/services/reducers";
+import { addCount, removeCount } from "../utils/services/reducers/reducers";
 const ads:FC = () => {
   let count = useSelector((state: any) => state?.counter?.value);
   let dispatch = useDispatch()
