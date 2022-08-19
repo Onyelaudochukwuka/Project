5840d21131252f0219c326d39565e9aa6556fd27
Udoka <udochukwukaonyela@gmail.com>
2022-08-19 12:52:40 +0100

    modification

--- utils/services/reducers/countrySlice.ts
+++ utils/services/reducers/countrySlice.ts
@@ -2,25 +2,23 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 // Define a type for the slice state
 interface CountryState {
-    state: string;
-    town: string;
+    state?: string;
 
 }
 
 // Define the initial state using that type
 const initialState: CountryState = {
     state: "ottawa",
-    town: "ottawa"
+
 }
 
  const countrySlice = createSlice({
     name: 'country',
-    // `createSlice` will infer the state type from the `initialState` argument
     initialState,
     reducers: {
         selectCountry(state: CountryState, action: PayloadAction<CountryState>) {
             state.state = action.payload.state;
-            state.town = action.payload.town;
+            // state.town = action.payload.town;
         },
     }
 })
