a4f994449a4f97e4f19bd3c9dbc56308cd0053a0
Udoka <udochukwukaonyela@gmail.com>
2022-08-27 16:16:37 +0100

    fixes

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,9 +1,65 @@
-import { useRouter } from 'next/router';
-// import type { NextPage } from "next";
-
-const Slug = () => {
+import Router, { useRouter } from 'next/router';
+import type { NextPage } from "next";
+import image from "../public/images.jpg";
+import { useEffect, useState } from "react";
+import { PostCardProps } from "../components/PostCard";
+const Slug: NextPage = () => {
   const params = useRouter();
   const { slug } = params.query;
+  const [slugAds, setSlugAds] = useState();
+  console.log(image)
+  const Data: { [k: string]: PostCardProps[] } = {
+    
+    Escort: [{
+      userName: "string",
+      timeOfPost: 2647384747,
+      image: image,
+      details: "i am a detail",
+    }, {
+      userName: "string",
+      timeOfPost: 2647384747,
+      image: image,
+      details: "i am a detail",
+    }, {
+      userName: "string",
+      timeOfPost: 2647384747,
+      image: image,
+      details: "i am a detail",
+    }, {
+      userName: "string",
+      timeOfPost: 2647384747,
+      image: image,
+      details: "i am a detail",
+      }],
+    Bicycle: [{
+      userName: "string",
+      timeOfPost: 2647384747,
+      image: image,
+      details: "i am a detail",
+    }, {
+      userName: "string",
+      timeOfPost: 2647384747,
+      image: image,
+      details: "i am a detail",
+    }, {
+      userName: "string",
+      timeOfPost: 2647384747,
+      image: image,
+      details: "i am a detail",
+    }, {
+      userName: "string",
+      timeOfPost: 2647384747,
+      image: image,
+      details: "i am a detail",
+    }]
+  }
+  useEffect(() => {
+    let data:string = typeof slug === "string" ? slug : "data";
+    if (!!Data[data]) {
+      setSlugAds(Data[slug])
+    }
+    console.log(slugAds)
+  }, [])
   return (
     <>
       {slug}
