f3729b73e9e956210f97c936872cdfaa8468782e
Udoka <udochukwukaonyela@gmail.com>
2022-08-27 16:40:36 +0100

    fixes

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,14 +2,12 @@ import Router, { useRouter } from 'next/router';
 import type { NextPage } from "next";
 import image from "../public/images.jpg";
 import { useEffect, useState } from "react";
-import { PostCardProps } from "../components/PostCard";
+import PostCard, { PostCardProps } from "../components/PostCard";
 const Slug: NextPage = () => {
   const params = useRouter();
   const { slug } = params.query;
   const [slugAds, setSlugAds] = useState<PostCardProps[]>();
-  console.log(image)
   const Data: { [k: string]: PostCardProps[] } = {
-    
     Escort: [{
       userName: "string",
       timeOfPost: 2647384747,
@@ -53,17 +51,25 @@ const Slug: NextPage = () => {
       details: "i am a detail",
     }]
   }
+
   useEffect(() => {
-    let data:string = typeof slug === "string" ? slug : "data";
+    let data: string = typeof slug === "string" ? slug : "data";
+    console.log(data);
     if (!!Data[data]) {
       setSlugAds(Data[data])
+      console.log(slugAds)
     }
-    console.log(slugAds)
   }, [])
+  console.log(slugAds)
+
   return (
-    <>
-      {slug}
-    </>
+    <div>
+      {
+        !!slugAds ? slugAds.map((val) => <PostCard {...val} />)
+          :
+          <p>No ads Available on this session</p>
+      }
+    </div>
   )
 }
 
