cfaaa250665301c03d267bece5afc3cccf9b0cc0
Udoka <udochukwukaonyela@gmail.com>
2022-08-27 13:44:32 +0100

    fixes

--- components/SelectorDropdown.tsx
+++ components/SelectorDropdown.tsx
@@ -3,15 +3,13 @@ import type { Pages } from "./Selectors";
 import { Dropdown } from "../public/icons";
 import SelectorDropItem from "./SelectorDropItem";
 const SelectorDropdown: FC<Pages & { i:number }> = ({ heading, value, icon,i }) => {
-    const [drop, setDrop] = useState<boolean>(false);
+  const [drop, setDrop] = useState<boolean>(false);
+
     const Id = useId();
-    useLayoutEffect(() => {
-    }, [])
+   
   return (
         <div className="transition-[height] duration-300 ease-in">
             <div
-                onBlur={() => setDrop(false)}
-                tabIndex={5}
                 onClick={() => setDrop(!drop)}
                 className="cursor-pointer bg-[#429EBD] py-3  rounded-lg flex items-center gap-2 px-8"
             >
