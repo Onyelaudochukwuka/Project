aedc4479eba70e7660d994eacaa2e4713ba569a5
Udoka <udochukwukaonyela@gmail.com>
2022-09-09 22:10:26 +0100

    added contents

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import React, { FC } from 'react'
-
-const Footer:FC = () => {
+const data = ["Home", "My Account", "Buy Credit", "Contact"];
+const termsNdPrimary = ["Privacy", "terms"];
+const Footer: FC = () => {
+  
   return (
-      <div className="footer">
-          <Link href={"/login"}>login</Link>
-    </div>
+      <footer className="w-full bg-medium flex flex-col p-8 items-center justify-center gap-8">
+      <div className="flex flex-col items-center justify-center gap-8">
+        {data.map((data) => <span key={data}>{data}</span>)}
+      </div>
+      <div className="flex gap-8">{ termsNdPrimary.map((data)=><span>{data}</span>) }</div>
+      </footer>
   )
 }
 
