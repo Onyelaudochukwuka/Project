16831b9a393dae8f9a2a8e7fa3268d9037ff74e4
Udoka <udochukwukaonyela@gmail.com>
2022-09-10 21:41:04 +0100

    styled text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,8 @@ const termsNdPrimary = ["Privacy", "terms"];
 const Footer: FC = () => {
   
   return (
-      <footer className="w-full bg-neutral flex flex-col p-8 items-center justify-center gap-8">
-      <div className="flex flex-col items-center justify-center gap-8">
+    <footer className="w-full bg-neutral text-light font-bold flex flex-col p-8 items-center justify-center gap-8">
+      <div className="flex flex-col items-center  justify-center gap-8">
         {data.map((data) => <span key={data}>{data}</span>)}
       </div>
       <div className="flex gap-8">{ termsNdPrimary.map((data)=><span>{data}</span>) }</div>
