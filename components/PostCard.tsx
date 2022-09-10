090a02443019bbc7c01b0d2fa1064989d67416c7
Udoka <udochukwukaonyela@gmail.com>
2022-09-10 21:41:33 +0100

    fixed post card

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -16,22 +16,27 @@ export interface PostCardProps {
 const PostCard: FC<PostCardProps> = ({ userName, timeOfPost, image, details }) => {
   const [state, setState] = useState(false);
   return (
-        <Link href={`user/${userName}`}>
-      <div className="text-light  cursor-pointer w-full relative bg-medium p-6 rounded-xl shadow-lg shadow-light/10 -ml-10">
+    <div className="text-neutral  cursor-pointer w-full relative ring-medium ring-2 p-6 shadow-lg shadow-light/10 -ml-10">
       <div className="flex items-center justify-between">
       <div className="flex items-center gap-6">
+              <Link href={`user/${userName}`}>
         <span className="block w-8 h-8 rounded-full">
           <Image src={image} className="rounded-full" layout="responsive" />
         </span>
-        <span>
-          <p>{userName}</p>
-          <p>{moment(timeOfPost).fromNow()}</p>
+          </Link>
+                <Link href={`user/${userName}`}>
+        <span className="flex items-center gap-2">
+              <p>{userName}</p>
+              <p>˚</p>
+          <p className="text-xs">{moment(timeOfPost).fromNow()}</p>
       </span>
+              </Link>
         </div>
         <div>
-          <div
-            onClick={() => setState(prev => !prev)} className={`relative w-2 h-2 rounded-full after:w-2 after:h-2 after:right-3 after:absolute after:rounded-full before:w-2 before:h-2 before:rounded-full before:bg-light before:right-6 before:absolute bg-light after:bg-light cursor-pointer after:cursor-pointer before:cursor-pointer`}></div>       
-            <div className={`absolute mt-4 right-0 bg-light text-dark py-4 rounded-xl ${!state ? `scale-y-0` : `scale-y-100`} transition-transform origin-center z-10`}>
+            <div
+              tabIndex={5}
+            onClick={() => setState(prev => !prev)} className={`relative w-1.5 h-1.5 rounded-full after:w-1.5 after:h-1.5 after:right-3 after:absolute after:rounded-full before:w-1.5 before:h-1.5 before:rounded-full before:bg-neutral before:right-6 before:absolute bg-neutral after:bg-neutral cursor-pointer after:cursor-pointer before:cursor-pointer`}></div>       
+            <div className={`absolute mt-4 right-0 text-light bg-neutral text-dark py-4 rounded-xl ${!state ? `scale-y-0` : `scale-y-100`} transition-transform origin-center z-10`}>
             <p className="px-8 py-2 cursor-pointer flex gap-2 items-center"><Disturb/> Not Interested</p>
             <p className="border-t-4 px-8 py-2 cursor-pointer flex gap-2 items-center"><Share /> Share</p>
             </div>
@@ -39,9 +44,10 @@ const PostCard: FC<PostCardProps> = ({ userName, timeOfPost, image, details }) =
         </div>
 
       </div>
-      <p className="pl-6 w-fit  mx-auto">{details}</p>
-    </div>
+      <Link href={`user/${userName}`}>
+      <p className="w-fit ml-6">{details}</p>
         </Link>
+    </div>
   );
 };
 
