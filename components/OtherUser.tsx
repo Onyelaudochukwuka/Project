613ae95974e7b14e0b6c3d37ab1bbf4fe6480532
Udoka <udochukwukaonyela@gmail.com>
2022-09-10 21:41:24 +0100

    fixed font color

--- components/OtherUser.tsx
+++ components/OtherUser.tsx
@@ -3,16 +3,16 @@ import React, { FC } from 'react'
 
 const OtherUser:FC<{img: StaticImageData, text: string}> = ({ img, text}) => {
   return (
-    <>
+    <div>
         <div className='w-9/12 flex flex-col gap-2 basis-1/3'>
         <div className='w-full' >
                 <Image src={img} alt="" layout="responsive"/>
             </div>
             <div>
-                <p className='text-light'>{text}</p>
+                <p className='text-neutral'>{text}</p>
             </div>
         </div>
-    </>
+    </div>
   )
 }
 
