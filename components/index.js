c681a471c4a3d8235ef2ca808a87c6cdfa9b07b7
Udoka <udochukwukaonyela@gmail.com>
2022-09-10 14:16:29 +0100

    exported components

--- components/index.js
+++ components/index.js
@@ -3,4 +3,5 @@ export { default as Layout } from './Layout';
 export { default as Footer } from './Footer';
 export { default as Modal } from './Modal';
 export { default as Selectors } from './Selectors';
+export { default as OtherUser } from './OtherUser';
 
