f5b797a26a9d83a6e68daf7db25638c94eade373
Udoka <udochukwukaonyela@gmail.com>
2022-09-10 14:17:56 +0100

    styles

--- pages/userdata.tsx
+++ pages/userdata.tsx
@@ -10,12 +10,36 @@ import { Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
+import OtherUser from "../components/OtherUser";
+
 const UserData = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const data = [
+    {
+      id: 1,
+      img: require("../public/section1.jpg"),
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fugiattemporibus atque cupiditate",
+    },
+    {
+      id: 2,
+      img: require("../public/section1.jpg"),
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fugiattemporibus atque cupiditate",
+    },
+    {
+      id: 3,
+      img: require("../public/section1.jpg"),
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fugiattemporibus atque cupiditate",
+    },
+    {
+      id: 4,
+      img: require("../public/section1.jpg"),
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fugiattemporibus atque cupiditate",
+    },
+    
+  ];
 
   return (
-
-    <div className="mx-auto px-10 my-20 w-full">
+    <div className="mx-auto px-10 lg:mx-[7rem] my-20 w-full ">
       <div className="flex gap-4 text-light items-center">
         <span className="block w-10 h-10 rounded-full">
           <Image src={pic} className="rounded-full" layout="responsive" />
@@ -36,21 +60,35 @@ const UserData = () => {
           </SwiperSlide>
           <SwiperSlide className="swiperSlide h-full">
             <img src={"/section2.jpg"} />
-          </SwiperSlide><SwiperSlide className="swiperSlide h-full">
+          </SwiperSlide>
+          <SwiperSlide className="swiperSlide h-full">
             <img src={"/section1.jpg"} />
           </SwiperSlide>
         </Swiper>
       </div>
-            <div>
+      <div>
         <p className="text-light pt-4 w-3/4 mx-auto">
           {/* 😍😍HELLO I AM A SEXY HOT AND BEAUTIFUL GIRL WRITE ME 100% GUARANTEED
           SERVICE💑💑 BBJ💦💦💦😋😋 KISS😘😘 MASSAGE AND GFE 😍😍😍💆♂💆♂
           👉👌😋😋💦💦💦if you are stressed or very tired come love and relax
           with me 🤤🥰 🍑 */}
-          BLaldkdbfdjbfjfbfffjdfvdsv hvfjdjhjchvcvhjvj j vvadvjjcdvva  jcajvkhajchv
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fugiat
+          temporibus atque cupiditate, suscipit laboriosam et aliquam molestias
+          nisi ullam magni sapiente minus sequi, illum eius. Sunt at sit omnis.
         </p>
       </div>
+      <div className="border-b-2 border-light pt-16 mb-4">
+        <p className="text-light">Related post</p>
+      </div>
+      <div className="grid md:grid-cols-3  lg:grid-cols-3 gap-4 ">
+          {data.map(({id ,...items}) => (
+            <OtherUser {...items} key={id} />
+          ))}
+        </div>
+      
+     
     </div>
+
   );
 };
 
