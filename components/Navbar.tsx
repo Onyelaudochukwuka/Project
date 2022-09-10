f16d6f44c21f020bc69f018b4da0d0cbff8abf45
Udoka <udochukwukaonyela@gmail.com>
2022-09-10 14:16:46 +0100

    styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,9 +25,9 @@ const Navbar: FC = () => {
     }
   }, []);
   return (
-    <section className="navbar">
-      <aside className={`${`${isDesktop ? `w-24 group absolute left-0 hover:w-56` : `absolute ${!isMobile ? `w-0 -left-full` : `w-24 left-0`} `}`} cursor-pointer transition-all duration-300 ease-in overflow-x-hidden  h-full min-h-screen left-0 top-0 bottom-0 z-10 flex bg-[#429EBD] justify-center flex-col  w-full items-center gap-16 content-center`}>
-          <Link href="/home" className="mx-auto">
+    <nav>
+      <aside className={`${`${isDesktop ? `w-24 group  left-0 hover:w-56` : `absolute ${!isMobile ? `w-0 -left-full` : `w-24 left-0`} `}`} cursor-pointer transition-all duration-300 ease-in overflow-x-hidden  h-full  left-0 top-0 bottom-0 z-10 flex bg-[#429EBD] justify-center gap-24 flex-col  w-full items-center content-center`}>
+          <Link href="/" className="mx-auto">
             <div className={`cursor-pointer ${isDesktop ? `gap-4 group-hover:flex` : `block`} `} onClick={() => setIsMobile (false)}>
               <span className="bg-[#9FE7F5] w-12 h-12 rounded-full flex items-center justify-center">
               <Home className="w-8 h-8 block" />
@@ -56,7 +56,7 @@ const Navbar: FC = () => {
       <div className={`bg-transparent relative top-4 lg:hidden ${!isMobile ? `left-4` : `left-56`} transition-all duration-500 ease-in`} onClick={navHandler} onBlur={() => setIsMobile(false)} tabIndex={24}>
           <span className="w-14 h-1 cursor-pointer rounded-full  bg-[#9FE7F5] block after:content-[''] after:relative before:w-12 after:h-1 after:bg-[#9FE7F5] after:block after:mt-1 after:float-right after:ml-auto before:content-[''] before:relative after:w-10 before:h-1 before:mt-2 before:float-right before:ml-auto before:bg-[#9FE7F5] before:block self-end before:self-end after:self-end before:rounded-full after:rounded-full"></span>
         </div>
-    </section>
+    </nav>
   );
 };
 
